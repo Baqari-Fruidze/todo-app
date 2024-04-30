b31761dc5cf5b6036c3e701c7f159f120c607e81
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-04-30 21:53:17 +0400

    sd

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,5 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 export const Global = createGlobalStyle`
+html{
+    font-size: 62.5%;
+}
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 body{
     background: black;
 }`;
