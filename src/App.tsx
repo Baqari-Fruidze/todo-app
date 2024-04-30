2a44c7bba6032f29d1d29b802cacdfdcb69ba8e5
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-04-30 19:56:13 +0400

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import cross from "/images/icon-cross.svg";
+import check from "/images/icon-check.svg";
+import moon from "/images/icon-moon.svg";
+import sun from "/images/icon-sun.svg";
+function App() {
+  return (
+    <>
+      <img src={cross} alt="" />
+      <img src={check} alt="" />
+      <img src={moon} alt="" />
+      <img src={sun} alt="" />
+    </>
+  );
+}
+
+export default App;
