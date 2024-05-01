3497a77608e09720bc4124b958a4aeb461af748a
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-01 20:22:57 +0400

    head

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,26 @@ import moon from "/images/icon-moon.svg";
 import sun from "/images/icon-sun.svg";
 import todo from "/images/TODO 2.svg";
 import mobileLight from "/images/bg-mobile-light.jpg";
+import mobileDark from "/images/bg-mobile-dark.jpg";
 import styled from "styled-components";
+import Header from "./Header";
+import { useState } from "react";
 function App() {
-  return <Parent>{/* <img src={mobileLigth} alt="" /> */}</Parent>;
+  const [theme, setTheme] = useState<boolean>(true);
+  return (
+    <Parent theme={theme}>
+      <Header theme={theme} setTheme={setTheme} />
+    </Parent>
+  );
 }
-const Parent = styled.div`
-  min-height: 100vh;
-  background-image: url(${mobileLight}) !important;
-  background: #fafafa;
+const Parent = styled.div<{ theme: boolean }>`
+  height: 100vh;
+  background-image: url(${(props) =>
+    props.theme ? mobileLight : mobileDark}) !important;
+  background-repeat: no-repeat !important;
+  background-size: 100% !important;
+  background: ${(props) => (props.theme ? "#FAFAFA" : " #171823;")};
+  padding: 4.8rem 2.4rem 7.2rem;
 `;
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import todo from "/images/TODO 2.svg";
+import moon from "/images/icon-moon.svg";
+import sun from "/images/icon-sun.svg";
+import styled from "styled-components";
+import { useState } from "react";
+
+export default function Header({
+  theme,
+  setTheme,
+}: {
+  theme: boolean;
+  setTheme: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const ThemeChanger = () => setTheme(!theme);
+  return (
+    <Head>
+      <img src={todo} alt="" />
+      {theme ? (
+        <img src={moon} alt="" onClick={ThemeChanger} />
+      ) : (
+        <img src={sun} alt="" onClick={ThemeChanger} />
+      )}
+    </Head>
+  );
+}
+const Head = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
